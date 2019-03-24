5126b2b7d44b3a02a2cc8d62ac27f042da0e0acb
Ali Sinan Coban <alisinancoban@gmail.com>
2019-03-24 17:17:41 +0300

    stylesheets and inline style

--- src/App.js
+++ src/App.js
@@ -35,12 +35,20 @@ class App extends Component {
   }
 
   render() {
+    //inline style
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    }
     return (
       <div className="App">
         <h1>Hi I m a React App</h1>
         <p>This is really working</p>
-        <button onClick = {this.switchNameHandler.bind(this, "Sinan")}>Performance Switch Name</button>
-        <button onClick = {() => this.switchNameHandler("Sinan!")}>Inefficient Switch Name</button>
+        <button style={style} onClick={this.switchNameHandler.bind(this, "Sinan")}>Performance Switch Name</button>
+        <button style={style} onClick = {() => this.switchNameHandler("Sinan!")}>Inefficient Switch Name</button>
         <Person 
           name = {this.state.persons[0].name} 
           age = {this.state.persons[0].age}/>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Person.css';
 
 //function type component
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick = {props.click}>I am {props.name} and {props.age} years old</p>
             <p>{props.children}</p>
-            <input type="text" onChange = {props.changed} value = {props.name}/>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
