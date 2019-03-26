8d0ed78e71d7610ab00b71f033d8704b9ec12070
Ali Sinan Coban <alisinancoban@gmail.com>
2019-03-26 21:47:39 +0300

    flexible list (spread opt. obj.assign, event )

--- src/App.js
+++ src/App.js
@@ -14,14 +14,26 @@ class App extends Component {
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+    //const person = Object.assign({}, this.state.persons[personIndex]);
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons: persons});
+    /*this.setState({
       persons: [
         {name: "Sinan", age: 23},
         {name: event.target.value, age: 21},
         {name: "Mustafa", age: 40}
      ]
-    })
+    })*/
   }
 
   deletePersonHandler = (personIndex) => {
@@ -59,6 +71,7 @@ class App extends Component {
               //click={() => this.deletePersonHandler(index)} 
               name={person.name} 
               age={person.age}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
               key={person.id} />
           })}
         </div>
