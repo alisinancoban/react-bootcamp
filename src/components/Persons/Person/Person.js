c2b02906cbd58c733f4ee1949e11bded7d3f1768
Ali Sinan Coban <alisinancoban@gmail.com>
2019-04-04 22:34:22 +0300

    better project structure and splitting components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,9 +4,9 @@ import styles from './Person.css';
 //function type component
 const person = (props) => {
     const random = Math.random();
-        if(random > 0.8){
+        /*if(random > 0.8){
             throw new Error("Something went wrong");
-        }
+        }*/
     return (
         <div className={styles.Person}>
             <p onClick = {props.click}>I am {props.name} and {props.age} years old</p>
