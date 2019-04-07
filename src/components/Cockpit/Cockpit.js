8cd9e12223991d7a4a51ee6d00c4333204de785e
Ali Sinan Coban <alisinancoban@gmail.com>
2019-04-07 18:35:34 +0300

    react.memo()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -27,10 +27,10 @@ const cockpit = (props) => {
         btnClass = styles.Red;
     }
 
-    if(props.persons.length <= 2){
+    if(props.personsLength <= 2){
       assignedClasses.push(styles.red);
     }
-    if(props.persons.length <= 1){
+    if(props.personsLength <= 1){
       assignedClasses.push(styles.bold);
     }
 
@@ -43,4 +43,4 @@ const cockpit = (props) => {
     );
 };
 
-export default cockpit;
+export default React.memo(cockpit);
